7d4b712e00833327dd7a17bd8430f0d8d1f08e0b
learnshare <learnshare@126.com>
2021-08-03 17:48:47 +0800

    ESLint and SCSS support.

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import './app.css';
+import './app.scss';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
